6ff2f63d79c248d57c2d103a5fac9d46ae09b083
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-03-07 15:14:36 -0300

    criando o componente Comment e a sua estilização

---
+++ src/components/Comment/Comment.tsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/RayanneRamos.png" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Rayanne Ramos</strong>
+              <time
+                title="01 de março às 15:14h"
+                dateTime="2023-03-01 15:14:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
