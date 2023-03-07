a5f45de7c7903b277049a4dc1c096e5b4361001e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-03-07 15:20:46 -0300

    importando o Avatar nos componentes que os utiliza

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "../Avatar/Avatar";
 import styles from "./Comment.module.css";
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/RayanneRamos.png" alt="" />
+      <Avatar src="https://github.com/RayanneRamos.png" alt="" />
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,3 +1,4 @@
+import { Avatar } from "../Avatar/Avatar";
 import { Comment } from "../Comment/Comment";
 import styles from "./Post.module.css";
 
@@ -6,10 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://avatars.githubusercontent.com/u/43352880?v=4"
-          />
+          <Avatar src="https://github.com/RayanneRamos.png" />
           <div className={styles.authorInfo}>
             <strong>Rayanne Ramos</strong>
             <span>Web Developer</span>
