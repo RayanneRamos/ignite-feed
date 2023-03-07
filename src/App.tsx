66f45c9bc9046fff786414581583648305e5678f
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-03-07 15:03:45 -0300

    importando os componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Header } from "./components/Header/Header";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+import styles from "./App.module.css";
 import "./global.css";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <p>fuweufewfhhewf</p>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
